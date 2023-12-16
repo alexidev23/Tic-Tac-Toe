2eaffcd4a09e1d57ffae910c81ca566318102c5f
AlexisDev <alexisescobar1234@gmail.com>
2023-12-16 16:46:39 -0300

    seccion home del juego

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,16 @@
-import { useState } from 'react'
-import confetti from 'canvas-confetti'
-import { Square } from './components/Square.jsx'
-import { TURNS } from './constants.js'
-import { checkWinner, checkEndGame } from './logic/board.js'
-import { WinnerModal } from './components/WinnerModal.jsx'
+import { Link } from 'react-router-dom'
+import TicTacToe from '../public/ticTacToe.svg'
 
 function App () {
-  // Construccion de nuestro tablero
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X)
-  // null no hay ganador, false hay empate
-  const [winner, setWinner] = useState(null)
-
-  const resetGame = () => {
-    setBoard(Array(9).fill(null))
-    setTurn(TURNS.X)
-    setWinner(null)
-  }
-
-  const updateBoard = (index) => {
-    // Esta funcion hace que no se sobraescriba el turno
-    if (board[index] || winner) return
-
-    // Actualizacion del tabkero
-    const newBoard = [...board] // Creamos una copia del array original
-    newBoard[index] = turn
-    setBoard(newBoard)
-
-    // Cambiar el turno
-    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
-    setTurn(newTurn)
-
-    // Revisamos si hay ganador
-    const newWinner = checkWinner(newBoard)
-    if (newWinner) {
-      confetti()
-      setWinner(newWinner)
-    } else if (checkEndGame(newBoard)) {
-      setWinner(false) // Verificamos si hubo un empate
-    }
-  }
-
   return (
-    <main className='board'>
+    <main className='home'>
       <h1>TIC TAC TOE</h1>
-      <button onClick={resetGame}>Empezar de nuevo</button>
-      <section className='turn'>
-        <Square isSelected={turn === TURNS.X}>
-          {TURNS.X}
-        </Square>
-        <Square isSelected={turn === TURNS.O}>
-          {TURNS.O}
-        </Square>
-      </section>
-      <section className='game'>
-        {board.map((_, index) => {
-          return (
-            <Square
-              key={index}
-              index={index}
-              updateBoard={updateBoard}
-            >
-              {board[index]}
-            </Square>
-          )
-        })}
-      </section>
-
-      <WinnerModal resetGame={resetGame} winner={winner} />
+      <div className='imagen'>
+        <img src={TicTacToe} alt='TicTacToe' height='100%' width='100%' />
+      </div>
+      <div className='boton'>
+        <Link to='/game' className='boton_jugar'>1 VS 1</Link>
+      </div>
     </main>
   )
 }
