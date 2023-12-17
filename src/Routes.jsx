da7338b5ef42444412824978d53e27cb7c475c65
AlexisDev <alexisescobar1234@gmail.com>
2023-12-16 22:11:03 -0300

    correcion en los imports

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from './App'
-import GameBoard from './game'
+import GameBoard from './Game'
 
 const Router = createBrowserRouter([
   {
